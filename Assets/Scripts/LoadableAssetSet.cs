017744019e48473a69003eb725ec2257420f5a66
WaterFriend <116010169@link.cuhk.edu.cn>
2022-04-17 17:44:04 +0800

    Optimize file path for release

--- Assets/Scripts/LoadableAssetSet.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using UnityEngine.AddressableAssets;
-
-[CreateAssetMenu(fileName = "LoadableAssetSet", menuName = "LoadableAssetSet", order = 0)]
-public class LoadableAssetSet : ScriptableObject
-{
-    public AssetReference[] assetRefs;
-}
